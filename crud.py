468c5135eab4cd3293e3925a62cea8c51e229d5f
Achilleas <achilleas.k@gmail.com>
2015-11-26 10:43:20 +0200

    CRUD operations for basic database

---
+++ crud.py
@@ -0,0 +1,33 @@
+import pymongo
+from pymongo import MongoClient
+
+
+class DatabaseManager(object):
+
+    def __init__(self):
+        self.client = MongoClient(host="localhost", port=27017)
+        self.database = self.client["todo"]
+
+    def create(self, item):
+        if item is not None:
+            self.database.todo.insert(item)
+        else:
+            pass  # TODO: raise exception
+
+    def read(self, item_id=None):
+        if item_id is None:
+            return self.database.todo.find()
+        else:
+            return self.database.todo.find({"_id": item_id})
+
+    def update(self, item):
+        if item is not None:
+            self.database.todo.save(item)
+        else:
+            pass  # TODO: raise exception
+
+    def delete(self, item):
+        if item is not None:
+            self.database.todo.remove(item)
+        else:
+            pass  # TODO: raise exception
