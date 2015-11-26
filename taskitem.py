736b8f0bb623e428ff00a04ab25784aa744fb8d8
Achilleas <achilleas.k@gmail.com>
2015-11-26 21:13:36 +0200

    Adding task name

--- taskitem.py
+++ taskitem.py
@@ -6,13 +6,14 @@ from bson.objectid import ObjectId
 
 
 class TaskItem(object):
-    def __init__(self, _id=None, user_id=None, description=None,
-                 date_due=None, priority=-1, done=False):
+    def __init__(self, _id=None, user_id=None, taskname=None,
+                 description=None, date_due=None, priority=-1, done=False):
         if _id is None:
             self._id = ObjectId()
         else:
             self._id = _id
         self.user_id = user_id
+        self.taskname = taskname,
         self.description = description
         self.date_due = date_due
         self.priority = priority
